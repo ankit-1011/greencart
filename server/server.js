5b8e2a2a647de95fb0ab54cfb7fc2a2a7280785e
Ankit Mishra <ankitmishra16127@gmail.com>
2025-11-10 14:51:13 +0530

    updaed

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ['http://localhost:5173','greencart-web.vercel.app'];
+const allowedOrigins = ['http://localhost:5173','https://greencart-web.vercel.app'];
 
 
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
